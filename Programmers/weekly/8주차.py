09bff38d5ab448ed110c8923f61fc17fb01e6b8e
subin <ansubin2004@gmail.com>
2021-10-15 22:57:55 +0900

    Solve Programmers

--- Programmers/weekly/8주차.py
+++ Programmers/weekly/8주차.py
@@ -1,12 +1,23 @@
- def solution(sizes):
-    cards = []
-    for a, b in sizes:
-        if a > b:
-            cards.append([a, b])
-        else:
-            cards.append([b, a])
+#  def solution(sizes):
+#     cards = []
+#     for a, b in sizes:
+#         if a > b:
+#             cards.append([a, b])
+#         else:
+#             cards.append([b, a])
     
-    max_a = max([c[0] for c in cards])
-    max_b = max([c[1] for c in cards])
+#     max_a = max([c[0] for c in cards])
+#     max_b = max([c[1] for c in cards])
     
-    return max_a * max_b
+#     return max_a * max_b
+
+# 배열에 저장 안하고 풀어보자!
+# 최소 중에 최대 X 최대 중에 최대만 만들면 됨
+def solution(sizes):
+    min_len = max_len = 0
+    for size in sizes:
+        a, b = sorted(size)
+        min_len = max(a, min_len)
+        max_len = max(b, max_len)
+    
+    return min_len * max_len
